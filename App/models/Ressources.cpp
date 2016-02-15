4b5038bf350e808560df41cda7dfe6dce839bbdb
Noki Dev <noki.dev@gmail.com>
2016-02-15 16:20:21 +0100

    Ressources Loader for textures

---
+++ App/models/Ressources.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Noki, Timothé Van Deputte on 15/02/2016.
+//
+
+#include "Ressources.hpp"
+
+Ressources Ressources::ressourcesLoader;
+
+sf::Texture &Ressources::Load(std::string textureName) {
+    if(!ressourcesLoader.exist(textureName))
+    {
+        ressourcesLoader.add(textureName);
+    }
+    return ressourcesLoader.get(textureName);
+}
+
+Ressources::~Ressources() {
+
+}
+
+
+sf::Texture &Ressources::get(std::string index) {
+    return textures[index];
+}
+
+void Ressources::add(std::string index) {
+    sf::Texture texture;
+    texture.loadFromFile("Ressources/"+ index);
+    textures[index] = texture;
+}
+
+bool Ressources::exist(std::string index) {
+    return textures.find(index) != textures.end();
+}
---
+++ App/models/Ressources.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Noki, Timothé Van Deputte on 15/02/2016.
+//
+
+#ifndef RPG_MVC_RESSOURCES_HPP
+#define RPG_MVC_RESSOURCES_HPP
+
+
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class Ressources {
+public:
+
+    ~Ressources();
+
+    static sf::Texture & Load(std::string textureName);
+   // static sf::Shader & LoadShader(std::string shader);
+
+private:
+    static Ressources ressourcesLoader;
+    std::map<std::string, sf::Texture> textures;
+    //std::map<std::string, sf::Shader> shaders;
+    sf::Texture &get(std::string index);
+
+    void add(std::string index);
+
+    bool exist(std::string index);
+};
+
+
+#endif //RPG_MVC_RESSOURCES_HPP
